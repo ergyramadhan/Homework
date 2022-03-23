1ddbff9bbfac4e02811bdee039f6b6209d54fbde
ergyramadhan <ramadhan.ergy@yahoo.com>
2022-03-23 15:37:20 +0700

    One

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './Data.js'
 
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <fieldset>
+        <legend className='fieldset'> Module 2 Session 1 </legend>
+        <img src={data.album.images[0].url} className="App-logo" alt="logo" />
+        <p> {data.name} </p>
+        <p> {data.album.name} </p>
+        <p> {data.artists[0].name} </p>
+        <a class="btn" href={data.album.artists[0].external_urls.spotify}>Select</a>
+          </fieldset>
       </header>
+      
     </div>
   );
 }
