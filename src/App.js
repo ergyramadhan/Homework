7b4e2a632a2f3e33033f8dfa1236a3e718426a3b
ergyramadhan <ramadhan.ergy@yahoo.com>
2022-03-28 12:14:19 +0700

    Update Modul 2 Seaason 3

--- src/App.js
+++ src/App.js
@@ -5,17 +5,49 @@ import Artists from './components/artists';
 import Button from './components/button';
 import Images from './components/images';
 
+const Row = ({ url, title, artist , album}) => {
+  return (
+    
+          <tr>
+            <td>{album}</td>
+            <td>{title}</td>
+            <td>{artist}</td>
+            <td><img src={url} alt=""  height='100' width='100' /></td>
+          </tr>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      
       <header className="App-header">
       <fieldset>
-        <legend className='fieldset'> Module 2 Session 2 </legend>
-        <Images image={data.album.images[0].url} title={data.name} />
+        <legend className='fieldset'> Homework </legend>
+        {/* <Images image={data.album.images[0].url} title={data.name} />
         <Album  album={data.album.name}/>
         <Artists artist={data.artists[0].name}/>
-        <Button tombol={data.album.artists[0].external_urls.spotify}/>
+        <Button tombol={data.album.artists[0].external_urls.spotify}/> */}
+
+        <table> 
+          <tr>
+            <th>Album</th>
+            <th>Title</th>
+            <th>Artist</th>
+            <th>Image</th>
+          </tr>
+        
+        {
+           data //.filter(item => item.rating !== "g")
+          .map(item => {
+            return (
+              <Row key="" url={item.album.images[0].url} title={item.name} album={item.album.name} artist={item.artists[0].name} />
+            )
+          })
+        }
+                  
+      </table>
+
+
           </fieldset>
       </header>
       
